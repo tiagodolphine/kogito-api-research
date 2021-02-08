abd6e1541fd557166c9da2032bf550908300a426
Edoardo Vacchi <evacchi@users.noreply.github.com>
2021-02-08 15:37:21 +0100

    refactored test

--- processes/core/src/test/java/org/kie/kogito/research/processes/core/impl/ProcessMessagingAPITest.java
+++ processes/core/src/test/java/org/kie/kogito/research/processes/core/impl/ProcessMessagingAPITest.java
@@ -1,28 +1,41 @@
 package org.kie.kogito.research.processes.core.impl;
 
 import org.junit.jupiter.api.Test;
+import org.kie.kogito.research.application.api.Event;
 import org.kie.kogito.research.application.api.SimpleRequestId;
 import org.kie.kogito.research.application.api.impl.SimpleId;
 import org.kie.kogito.research.application.core.impl.BroadcastProcessorMessageBus;
 import org.kie.kogito.research.processes.api.messages.ProcessMessages;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProcessMessagingAPITest {
 
     @Test
-    public void createInstance() {
+    public void createInstance() throws InterruptedException, ExecutionException {
+        // set up the system (internal APIs)
         var messageBus = new BroadcastProcessorMessageBus();
-        var process = new ProcessImpl(null, SimpleProcessId.fromString("my.process"), messageBus);
-        var createInstance = ProcessMessages.CreateInstance.of(new SimpleRequestId(), SimpleProcessId.fromString("my.process"));
+        var processId = SimpleProcessId.fromString("my.process");
+        var process = new ProcessImpl(null, processId, messageBus);
+
+        // create instance via message passing
         var selfId = new SimpleId();
+        var requestId = new SimpleRequestId();
+        var createInstance = ProcessMessages.CreateInstance.of(requestId, processId);
         messageBus.send(new SimpleProcessEvent(selfId, null, createInstance));
-        messageBus.subscribe(e -> {
-            if (e.targetId() == selfId) {
-                assertEquals(ProcessMessages.InstanceCreated.class, e.payload().getClass());
-                System.out.println(e);
-            }
-        });
+
+        // await confirmation
+        var instanceCreatedFuture = messageBus.processor()
+                .filter(e -> e.targetId() == selfId)
+                .map(Event::payload)
+                .map(ProcessMessages.InstanceCreated.class::cast)
+                .toUni().subscribeAsCompletionStage();
         process.run();
+        var instanceCreated = instanceCreatedFuture.get();
+        assertEquals(processId, instanceCreated.processId());
+        assertEquals(requestId, instanceCreated.requestId());
     }
 }
